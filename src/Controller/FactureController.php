b8280508225b9dadb7ecfdbaf04b3f4e94fa099c
EmmanueljPrime <emmanueljprime@gmail.com>
2024-12-19 16:01:37 +0100

    Ajout Pagination factures

--- src/Controller/FactureController.php
+++ src/Controller/FactureController.php
@@ -10,26 +10,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Knp\Component\Pager\PaginatorInterface;
 
 #[Route('/factures')]
 final class FactureController extends AbstractController
 {
     #[Route(name: 'facture_index', methods: ['GET'])]
-    public function index(Request $request, FactureRepository $factureRepository): Response
+    public function index(Request $request, FactureRepository $factureRepository, PaginatorInterface $paginator): Response
     {
-        // Récupérer le terme de recherche
         $searchTerm = $request->query->get('q');
+        $itemsPerPage = $request->query->getInt('items_per_page', 10);
 
-        // Filtrer les factures en fonction du terme
+
         if ($searchTerm) {
             $factures = $factureRepository->findBySearchTerm($searchTerm);
         } else {
             $factures = $factureRepository->findAll();
         }
 
+        $pagination = $paginator->paginate(
+            $factures,
+            $request->query->getInt('page', 1),
+            $itemsPerPage
+        );
+
         return $this->render('facture/index.html.twig', [
-            'factures' => $factures,
+            'factures' => $pagination,
             'searchTerm' => $searchTerm,
+            'itemsPerPage' => $itemsPerPage,
         ]);
     }
 
